19280e61819231be75e1558e67aeee7c21f47e1b
lizeyliz <alizaeaton@gmail.com>
2025-01-17 08:48:55 -0800

    iteration case for even words

--- AlizaPalindrome.java
+++ AlizaPalindrome.java
@@ -14,15 +14,31 @@ public class AlizaPalindrome {
         String word5 = "foot";
         String word6 = "toot";
 
-        isPalindrome(word1);
-        isPalindrome(word2);
+        isPalindrome(word1);//works
+        isPalindrome(word2);//works
+
+        //even not palindrome
+        isPalindrome(word5);
 
 
     }//end main
     public static void isPalindrome(String word) {
         //add case for no word entered
-        if (word.length() == 1 || word.length() == 2) {
+        word.toLowerCase();
+        if(word.length() == 1) {
             System.out.println(word + " is not a palindrome.");
+        }
+        //case EVEN
+        if(word.length() % 2 == 0){
+            for (int i = 0; i < word.length()/2; i++) {
+                //chars don't match
+                if(word.charAt(i) != word.charAt(word.length()-(i+1))){
+                    System.out.println(word + " is not a palindrome.");
+                } 
+                
+            }
+        } else { //case ODD
+
         }
     }//end isPalindrome
 }
