3e7b5183eeeb63068de0cc3e27bd9a6ff54cd142
lizeyliz <alizaeaton@gmail.com>
2025-01-17 08:23:43 -0800

    aliza's method

---
+++ AlizaPalindrome.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+//to lowercase
+public class AlizaPalindrome {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        /*System.out.println("Enter a word and I will check if it's a palindrome.");
+        String word = scan.next;*/
+    }
+    public void isPalindrome(String word) {
+        if (word.length() == 1) {
+            
+        }
+    }
+}
