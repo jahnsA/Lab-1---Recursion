f3ef04324b7a3552f1755c4c2cebc3532a6f749b
lizeyliz <alizaeaton@gmail.com>
2025-01-18 07:59:35 -0800

    fixed single letter error

--- PalindromeC.c
+++ PalindromeC.c
@@ -1,5 +1,6 @@
 //W3 Schools C
 //https://www.geeksforgeeks.org/tolower-function-in-c/?ref=ml_lbp
+//https://www.programiz.com/c-programming/library-function/string.h/strlen
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
@@ -15,12 +16,21 @@ void isPalindromeHelper(int left, int right, char* word){
     isPalindromeHelper(left+1, right-1, word);
 }//end isPalindromeHelper
 void isPalindrome(char* word) {
-//add in lower case word later
     if(strlen(word) <= 1) {
-        printf("%s is not a palindrome\n", word);//fix to add string later
+        printf("%s is not a palindrome\n", word);
+        return;
     }//end if statement
     isPalindromeHelper(0, strlen(word)-1, word);
 }//end isPalindrome
 int main(){
+    //add user input and to lowercase here
+    //TEST cases
+    isPalindrome("a");
+    isPalindrome("at");
     isPalindrome("dad");
-}
+    isPalindrome("tea");
+    isPalindrome("toot");
+    isPalindrome("book");
+    isPalindrome("madam");
+    isPalindrome("creak");
+}//end main
